3d899fddc857dc210452c5fbcf531accf8747b82
Federico Toscano <federicotoscano611@gmail.com>
2026-03-27 00:09:01 +0100

    update

--- code/ascii/ex01.c
+++ code/ascii/ex01.c
@@ -4,7 +4,7 @@ int main(void){
 	char *s = "Hello World!";
 	int i = 0;
 
-	while(s[i] != '/0'){
+	while(s[i] != '\0'){
 		write(1, &s[i], 1);
 		i++;
 	}
---
+++ code/ascii/ex02.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+void print_ascii_digits(int range){
+	for(int i = 0; i <= range; i++){
+
+	}
+}
+
+int main(void){
+	// print_ascii_digits(9);
+	char *a = '65';
+	write(1, &a, 1);
+	return 0;
+}
