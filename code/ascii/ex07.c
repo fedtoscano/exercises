957cb00b3a79e08a705171fd9149e2aa6ebbf758
Federico Toscano <alessandro.aliberti@polito.it>
2026-03-27 11:32:59 +0100

    u

--- code/ascii/ex07.c
+++ code/ascii/ex07.c
@@ -2,15 +2,13 @@
 #include <stdio.h>
 
 void from_ascii_to_digit(char c){
+  //converting char to int
 	int n = c;
 	write(1, &n, 4);
 }
 
+//a char is a 1byte number that is mapped to ASCII table
 int main(void){
-	for(int i = 0; i <=9; i++){
-		char a = i;
-		printf("%c\n", a);
-		from_ascii_to_digit(a);
-	}
+	for(char c = '0'; c <= '9'; c++) from_ascii_to_digit(c);
 	return 0;
 }
