bb5e4290cbd47207671da68b857ac303cf17ab0e
Federico Toscano <federicotoscano611@gmail.com>
2026-03-28 20:54:56 +0100

    u

--- code/ascii/ex06.c
+++ code/ascii/ex06.c
@@ -5,14 +5,11 @@ void from_digit_to_ascii(int digit){
 	write(1, &digit, 1);
 }
 
-<<<<<<< HEAD
-=======
 void from_ascii_to_digit(char c){
 	int digit = c - '0';
 	write(1, &digit, 1);
 }
 
->>>>>>> 51f0839 (update)
 int main(void){
 	for(int i = 0; i <=9; i++)
 		from_digit_to_ascii(i);
