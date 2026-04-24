9dae4c540c91d64516cf8a6a1b100a0da9c1d431
Federico Toscano <federicotoscano611@gmail.com>
2026-04-24 15:17:15 +0200

    double_linked

---
+++ code/c/linked-list/double-linked.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+	int value;
+	struct node *prev;
+	struct node *next;
+} node;
+
+node *node_init(int value){
+	node *n = malloc(sizeof(node));
+	if(n == NULL){
+		printf("n points to NULL!\n");
+		return NULL;
+	}
+
+	n->value = value;
+	n->prev =  NULL;
+	n->next =  NULL;
+	return n;
+}
+
+void append_before(node *to, int value){
+
+}
+
+void append_after(node *to, int value){
+
+}
+
+void print_node(node *n);
+void delete_node(node *n);
+node *node_search(int value);
+
+int main(void){
+
+	return 0;
+}
