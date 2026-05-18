f126c293ddedf91f585f35b19ead4fc67a241f29
Federico Toscano <federicotoscano611@gmail.com>
2026-05-18 16:15:56 +0200

    fixed memory accesses and recursion

--- code/c/n-queens/n-queens.c
+++ code/c/n-queens/n-queens.c
@@ -1,36 +1,21 @@
 #include <stdio.h>
 
-/*
- *
- * {
- *  {X, 0, 0, 0},
- *  {0, 0, 0, 0},
- *  {0, 0, 0, 0},
- *  {0, 0, 0, 0},
- * }
- *
- * */
+#define N 4
 
-#define N 8
 typedef int Board[N][N];
 
-int queen_is_safe(Board *b, int x, int y) {
-  for (int i = 0; i < N; i++) {
-    // horizontal
-    if (b[x][i])
-      return 0;
-
-    // vertical
-    if (b[i][y])
-      return 0;
-
-    // diagonal
-    int d1 = x + y - 1;
-    if(d1 >= 0 && d1 < N && b[i][d1]) return 0;
-
-    //diagonal
-    int d2 = i + y - x;
-    if(d1 >= 0 && d2 < N && b[i][d2]) return 0;
+int queen_is_safe(Board b, int row, int col) {
+  // horizontal
+  for (int i = 0; i < col; i++) {
+    if (b[row][i]) return 0;
+  }
+  // diagonal up-left
+  for (int i = 1; row - i >= 0 && col - i >= 0; i++) {
+    if (b[row - i][col - i]) return 0;
+  }
+  // diagonal down-left
+  for (int i = 1; row + i < N && col - i >= 0; i++) {
+    if (b[row + i][col - i]) return 0;
   }
 
   return 1;
@@ -45,24 +30,34 @@ void print_board(Board b) {
   }
 }
 
-int n_queens() {
-  Board b;
-  for (int i = 0; i < N; i++) {
-    int y = 0;
-    // place queen
-    b[i][y] = 1;
-    while (!queen_is_safe(&b, i, y)) {
-      // move the queen from left to right
-      b[i][y] = 0;
-      b[i][++y] = 1;
-    }
-  }
+int solve(Board b, int col) {
+  // if col is greater than number of cols (4 in our case), return
+  if (col >= N) return 1;
 
-  print_board(b);
+  //we check every row for given col
+  for (int row = 0; row < N; row++) {
+    if (queen_is_safe(b, row, col)) {
+
+      //place a queen
+      b[row][col] = 1;
+      
+      //if the queen is safe, skip iteration
+      if (solve(b, col + 1)) return 1;
+
+      // otherwise, clear the house and place queen next house
+      b[row][col] = 0;
+    }
+
+    // if queen is not safe increase row counter
+  }
   return 0;
 }
 
 int main() {
-  n_queens();
+  Board b = {0};
+  // we start from col = 0
+  int solved = solve(b, 0);
+  if(solved) print_board(b);
+  else printf("Queen board was not solved!\n");
   return 0;
 }
