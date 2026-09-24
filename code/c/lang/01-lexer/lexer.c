e4057c6d9f117b5a191260b882097fc503247092
Federico Toscano <federicotoscano611@gmail.com>
2026-09-24 15:54:06 +0200

    refined error checks

--- code/c/lang/01-lexer/lexer.c
+++ code/c/lang/01-lexer/lexer.c
@@ -2,13 +2,20 @@
 #include <stdlib.h>
 
 FILE *get_file_stream(char *name) {
-  FILE *f = fopen(name, "rb");
+  FILE *f = fopen(name, "rb"); // mode: read bynary
   return f;
 }
 
 long get_file_size(FILE *f){
-  fseek(f, 0, SEEK_END);
+  // moves the cursor from start to the end
+  // of the file
+  int end = fseek(f, 0, SEEK_END);
+  if(end == -1){
+    return end;
+  }
+  // how many bytes are there
   long size = ftell(f);
+  // return cursor to the start
   fseek(f, 0, SEEK_SET);
   return size;
 }
@@ -32,17 +39,37 @@ int main(int argc, char *argv[]) {
     return 2;
   }
 
+  // recast file size as a size_t 
+  // (is the type accepted by malloc())
   size_t size = (size_t)size_tmp;
 
+  // + 1 because we need the '\0' char at the end 
   char *buf = malloc(size + 1);
   if(buf == NULL){
     perror("Cannot allocate memory!");
+    fclose(f);
     return 2;
   }
 
+  // reads into the filestream (f), 1 byte at the time
+  // and copies into buf for size times
   size_t flen = fread(buf, 1, size, f);
+  if(flen < size){
+    if(ferror(f) != 0){
+      fprintf(
+          stderr, 
+          "Impossible to read file: some connection error occurred\n"
+          );
+      fclose(f);
+      free(buf);
+      return 2;
+    }
+  }
+
+  // adds the null terminator
   buf[flen] = '\0';
 
+  // prints out the buffer in stdio
   for(size_t i = 0; i < flen; i++){
     printf("%c", buf[i]);
   }
